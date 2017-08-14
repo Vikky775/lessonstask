9a7e063c057600f9c8244eff6e97941e18eee98c
Viktoria <viktoria.galak.95@gmail.com>
2017-08-14 20:02:27 +0300

    comment

---
+++ lesson/Program1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)/*найти два максимальных элемента массива*/
+        {
+            Console.WriteLine("Задайте динамиескую размерность массива: ");
+            int arLength = int.Parse(Console.ReadLine());
+
+            int[] massiv = new int[arLength];
+
+            for (int a = 0; a < massiv.Length; a++)
+            {
+                Console.WriteLine("Введите число " + (a + 1) + ": ");
+                massiv[a] = int.Parse(Console.ReadLine());
+            }
+            int max1 = 0;
+            int max2 = 0;
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                if (massiv[i] > max1)
+                {
+                    max1 = massiv[i];
+                }
+            }
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                if (massiv[i] > max2 && massiv[i] < max1)
+                {
+                    max2 = massiv[i];
+                }
+            }
+            Console.WriteLine("Первое максимальное число равно " + max1 + " , второе максимальное число - " + max2 + ".");
+        }
+    }
+}
---
+++ lesson/Program2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryHomework
+{
+    class Program
+    {
+        static void Main(string[] args) /* система ведения учета книг.  С помощью while true зацикливаем, 
+            создаем бесконечный цикл, чтобы выйти из цикла - используем return,  */
+        {
+        }
+        public static string Login()
+        {
+            Console.WriteLine("Введите пароль: "); //user - 111. admin - 000
+            String password = Console.ReadLine();
+            if (password == "111") { }
+            else if (password == "000") { }
+            else
+            {
+                Console.WriteLine("Неверный пароль. Попробуйте еще раз.");
+            }
+            return "";
+        }
+        public static string Admin_Menu()
+        {
+            Console.WriteLine("Выберите действие: " +
+                "Чтобы добавить книгу, нажмите 1" +
+                "Чтобы удалить книгу, нажмите 2 ");
+            String admin = Console.ReadLine();
+            if (admin == "1") { }
+            else if (admin == "2") { }
+            else { Console.WriteLine("Вы выбрали несуществующее действие.");}
+            return "";
+        }
+        public static string User_Menu()
+        {
+            Console.WriteLine("Выберите действие:" +
+                "Если Вы хотите просмотреть список книг - нажмите 1" +
+                "Если Вы хотите взять книгу - нажмите 2" +
+                "Если Вы хотите вернуть книгу - нажмите 3");
+            string user = Console.ReadLine();
+            if (user == "1") { }
+            else if (user == "2") { }
+            else if (user == "3") { }
+            else
+            {
+                Console.WriteLine("Вы выбрали несуществующее действие.");
+            }
+            return "";
+        }
+        public static string BooksList() // сделать действие - "Если хотите выйти в меню - нажмите 0, для админа и юзера - отдельно.
+        {
+            int[] book = new int[5];//не динамический массив
+            book[0] = book1;
+            book[1] = book2
+            book[2] = book3;
+            book[3] = book4;
+            book[4] = book5;
+            // должен быть динамический массив, чтобы его можно было принять из метода и его выводить на экран. 
+            //для того, чтобы админ мог удалять/добавлять книги пользователь может брать/возвращать книги.          
+            return "";
+        }
+         
+        //  while (true) //создаем цикл 
+        //    result = Login()  //показываем результат 
+
+    }
+}
